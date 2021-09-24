22b797fe8d6917dd9deb2e477c6b630358558845
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-24 18:39:50 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:face_detection_app/screens/face_detection_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FaceDetectionScreen(),
+  ));
+}
---
+++ lib/screens/face_detection_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FaceDetectionScreen extends StatefulWidget {
+  @override
+  _FaceDetectionScreenState createState() => _FaceDetectionScreenState();
+}
+
+class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
