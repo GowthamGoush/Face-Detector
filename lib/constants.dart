1db1eebeb91a3575d01c3e11352cd6cdf9f16f6c
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-25 10:56:30 +0530

    Add constants dart file

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+const String _imageUrl =
+    "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=600x600";
+
+const transBlackColor = Colors.black26;
+
+const faceDetectScreenDecoImage =
+    DecorationImage(image: NetworkImage(_imageUrl), fit: BoxFit.cover);
+
+const faceDetectScreenBoxShadowChild = BoxShadow(
+  color: transBlackColor,
+  spreadRadius: 5,
+  blurRadius: 10,
+  offset: Offset(0, 5),
+);
