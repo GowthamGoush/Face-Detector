35fa595f2ba7c897aa455f06d22ef1c992963540
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-28 10:13:50 +0530

    Update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,9 +11,28 @@ const circularRadius = Radius.circular(32.0);
 const faceDetectScreenDecoImage =
     DecorationImage(image: NetworkImage(_imageUrl), fit: BoxFit.cover);
 
+const uploadText = Text(
+  "Upload",
+  style: TextStyle(fontFamily: 'Spartan', fontSize: 18),
+);
+
+const submitText = Text(
+  "Submit",
+  style: TextStyle(fontFamily: 'Spartan', fontSize: 18),
+);
+
 const faceDetectScreenBoxShadowChild = BoxShadow(
   color: transBlackColor,
   spreadRadius: 5,
   blurRadius: 10,
   offset: Offset(0, 5),
 );
+
+const containerGradient = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: [
+      Color(0xff654ea3),
+      Color(0xffeaafc8),
+    ],
+    tileMode: TileMode.clamp);
